7de9f1ae8e6b3caf5e46597f554205075dce3346
Bombel <bombel@bombel.com>
2020-01-16 18:40:45 +0100

    Make processOrder()

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -63,6 +63,8 @@
       thisProduct.renderInMenu();
       thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
 
       console.log('newProduct: ', thisProduct);
     }
@@ -118,7 +120,47 @@
       /* END: click event listener to trigger */
       });
     }
-  }
+
+    initOrderForm() {
+      const thisProduct = this;
+      console.log(thisProduct);
+    }
+    
+    processOrder(){
+      const thisProduct = this;
+    
+      /* read all data from the form (using utils.serializeFormToObject) and save it to const formData */
+      const formData = utils.serializeFormToObject(thisProduct.form);
+      console.log('formData: ', formData);
+      /* set variable price to equal thisProduct.data.price */
+      const price = thisProduct.element.querySelector(thisProduct.data.price);
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for (let paramId in paramsId) {
+        /* save the element in thisProduct.data.params with key paramId as const param */
+        const paramsId = thisProduct.element.querySelector(thisProduct.data.params);
+        /* START LOOP: for each optionId in param.options */
+        for (let paramId in paramIdOptions) {
+          /* save the element in param.options with key optionId as const option */
+          const paramIdOptions = thisProduct.element.querySelector(param.options);
+          /* START IF: if option is selected and option is not default */
+          if (paramIdOptions == !option.default) {
+            /* add price of option to variable price */
+            price =+ paramIdOptions;
+          /* END IF: if option is selected and option is not default */
+          }
+          /* START ELSE IF: if option is not selected and option is default */
+          if (paramIdOptions == option.default) {
+            /* deduct price of option from price */
+            price =- paramIdOptions;
+          /* END ELSE IF: if option is not selected and option is default */
+          }
+        /* END LOOP: for each optionId in param.options */
+        }
+      /* END LOOP: for each paramId in thisProduct.data.params */
+      }
+      /* set the contents of thisProduct.priceElem to be the value of variable price */
+      price = thisProduct.element.querySelector(thisProduct.priceElem);
+    }
   
   const app = {
     
