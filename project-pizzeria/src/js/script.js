9fe33100d63ad39d85c2b8b341f5666b4b6be42d
Bombel <bombel@bombel.com>
2020-01-18 17:04:35 +0100

    Corect structure function processOrder()

--- project-pizzeria/src/js/script.js
+++ project-pizzeria/src/js/script.js
@@ -146,44 +146,44 @@
       /* set variable price to equal thisProduct.data.price */
       let price = thisProduct.data.price;
 
-        /* START LOOP: for each paramId in thisProduct.data.params */
-        for (let paramId in thisProduct.data.params) {
-          console.log('paramId: ', paramId);
-        
+      /* START LOOP: for each paramId in thisProduct.data.params */
+      for (let paramId in thisProduct.data.params) {
+        console.log('paramId: ', paramId);
+
 
         /* save the element in thisProduct.data.params with key paramId as const param */
         const param = thisProduct.data.params;
         console.log('param: ', param);
 
-          /* START LOOP: for each optionId in param.options */
-          for (let optionId in param.options) {
+        /* START LOOP: for each optionId in param.options */
+        for (let optionId in param.options) {
 
           /* save the element in param.options with key optionId as const option */
-            const option = param.options;
-            console.log('option: ', option);
+          const option = param.options;
+          console.log('option: ', option);
 
-            /* START IF: if option is selected and option is not default */
-            const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
-            if(optionSelected && !option.default) {
+          /* START IF: if option is selected and option is not default */
+          const optionSelected = formData.hasOwnProperty(paramId) && formData[paramId].indexOf(optionId) > -1;
+          if(optionSelected && !option.default) {
 
             /* add price of option to variable price */
-              price += optionSelected;
-              console.log('price: ', price);
+            price += optionSelected;
+            console.log('price: ', price);
 
-            /* END IF: if option is selected and option is not default */
-            }
-            /* START ELSE IF: if option is not selected and option is default */
-            if(optionSelected && option.default) {
+          /* END IF: if option is selected and option is not default */
+          }
+          /* START ELSE IF: if option is not selected and option is default */
+          if(optionSelected && option.default) {
 
             /* deduct price of option from price */
             price -= optionSelected;
 
-            /* END ELSE IF: if option is not selected and option is default */
-            }
-          /* END LOOP: for each optionId in param.options */
+          /* END ELSE IF: if option is not selected and option is default */
           }
-        /* END LOOP: for each paramId in thisProduct.data.params */
+          /* END LOOP: for each optionId in param.options */
         }
+        /* END LOOP: for each paramId in thisProduct.data.params */
+      }
       /* set the contents of thisProduct.priceElem to be the value of variable price */
       price[thisProduct.priceElem];
     }
