628392fec71d2d9b9830363f0e0d45a25ad9d0c8
Bombel <bombel@bombel.com>
2020-01-31 23:01:20 +0100

    Add BaseWidgets

---
+++ src/js/components/BaseWidget.js
@@ -0,0 +1,48 @@
+
+class BaseWidget {
+  constructor(wrapperElement, initialValue) {
+    const thisWidget = this;
+
+    thisWidget.dom.wrapper(wrapperElement);
+    thisWidget.dom.thisWidget.correctValue(initialValue);
+  }
+
+  get value() {
+    const thisWidget = this;
+
+    return thisWidget.correctValue;
+  }
+
+  set value(assignedValue) {
+    const thisWidget = this;
+
+    const newValue = thisWidget.parseValue(assignedValue);
+
+    if (newValue != thisWidget.correctValue && thisWidget.isValid(newValue)) {
+      thisWidget.correctValue = newValue;
+      thisWidget.announce();
+    }
+
+    thisWidget.renderValue();
+  }
+
+  parseValue(newValue) {
+    return !isNaN(newValue);
+  }
+
+  renderValue() {
+    const thisWidget = this;
+
+    thisWidget.dom.input.value = thisWidget.value;
+  }
+
+  announce() {
+    const thisWidget = this;
+
+    const event = new CustomEvent('update', {
+      bubbles: true
+    });
+
+    thisWidget.dom.wrapper.dispatchEvent(event);
+  }
+}
