bafdb4a2952310800ce8cd5a88a16306a18b8168
Bombel <bombel@bombel.com>
2020-02-03 08:51:01 +0100

    Add getDate() and parseDate() and change in app.json repeat: false for repeat: 2020-02-02

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -1,4 +1,4 @@
-import {templates, select} from './../settings.js';
+import {templates, select, settings} from './../settings.js';
 import {utils} from './../utils.js';
 import {AmountWidget} from './AmountWidget.js';
 import {DatePicker} from './DatePicker.js';
@@ -10,7 +10,56 @@ export class Booking{
  
     thisBooking.render(reservWidgetContainer);
     thisBooking.initWidgets();
- 
+    thisBooking.getData();
+    thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
+  }
+
+  getData(){
+    const thisBooking = this;
+  
+    const startEndDates = {};
+    startEndDates[settings.db.dateStartParamKey] = utils.dateToStr(thisBooking.datePicker.minDate);
+    startEndDates[settings.db.dateEndParamKey] = utils.dateToStr(thisBooking.datePicker.maxDate);
+  
+    const endDate = {};
+    endDate[settings.db.dateEndParamKey] = startEndDates[settings.db.dateEndParamKey];
+  
+    const params = {
+      booking: utils.queryParams(startEndDates),
+      eventsCurrent: settings.db.notRepeatParam + '&' + utils.queryParams(startEndDates),
+      eventsRepeat: settings.db.repeatParam + '&' + utils.queryParams(endDate),
+    };
+  
+    console.log('getData params', params);
+  }
+
+  parseData(bookings, eventsCurrent, eventsRepeat) {
+    const thisBooking = this;
+    thisBooking.booked = {};
+
+    console.log('eventsCurrent: ', eventsCurrent);
+
+    const urls = {
+      booking: settings.db.url + '/' + settings.db.booking + '?' + params.booking,
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent,
+      eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat,
+    };
+
+    Promise.all([
+      fetch(urls.booking), // 5s
+      fetch(urls.eventsCurrent), // 3s
+      fetch(urls.eventsRepeat), // 2s
+    ])
+      .then(function([bookingsResponse, eventsCurrentResponse, eventsRepeatResponse]){
+        return Promise.all([
+          bookingsResponse.json(),
+          eventsCurrentResponse.json(),
+          eventsRepeatResponse.json(),
+        ]);
+      })
+      .then(function([bookings, eventsCurrent, eventsRepeat]){
+        thisBooking.parseData(bookings, eventsCurrent, eventsRepeat);
+      });
   }
  
   render(bookingContainer){
