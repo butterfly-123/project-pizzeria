76aeb0e3ffa56f6ca20c586a0038d474c896ed67
Bombel <bombel@bombel.com>
2020-02-07 18:47:00 +0100

    Add change in my code

--- src/js/components/Booking.js
+++ src/js/components/Booking.js
@@ -5,9 +5,7 @@ import {DatePicker} from './DatePicker.js';
 import {HourPicker} from './HourPicker.js';
 
 export class Booking {
-  
-  // Z kad jest wzięty ten argument w constructor i co on ma za zadanie?
-  constructor(element){
+  constructor(element) {
     const thisBooking = this;
  
     thisBooking.render(element);
@@ -15,34 +13,31 @@ export class Booking {
     thisBooking.getData();
   }
 
-  getData(){
+  getData() {
     const thisBooking = this;
  
-    // Rozumiem, ze ta zmienna przypisuje z utils poczatkową i koncowa date?
     const startDateParam = settings.db.dateStartParamKey + '=' + utils.dateToStr(thisBooking.datePicker.minDate);
     const endDateParam = settings.db.dateEndParamKey + '=' + utils.dateToStr(thisBooking.datePicker.maxDate);
  
     const params = {
       booking: [startDateParam, endDateParam],
-      eventCurrent:[settings.db.notRepeatParam, startDateParam, endDateParam],
-      eventsRepeat:[settings.db.repeatParam, endDateParam],
+      eventsCurrent: [settings.db.notRepeatParam, startDateParam, endDateParam],
+      eventsRepeat: [settings.db.repeatParam, endDateParam]
     };
  
-    console.log('getData params', params);
+    // console.log('getData params', params);
  
-    // To są adresy filtrujące dane, tak? Co to oznacza?
     const urls = {
       booking:      settings.db.url + '/' + settings.db.booking + '?' + params.booking.join('&'),
-      eventCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventCurrent.join('&'),
+      eventsCurrent: settings.db.url + '/' + settings.db.event + '?' + params.eventsCurrent.join('&'),
       eventsRepeat: settings.db.url + '/' + settings.db.event + '?' + params.eventsRepeat.join('&'),
     };
 
     // console.log('urls: ', urls);
  
-    // Promise - wysyła 3 prozby o rezerwacje
     Promise.all([
       fetch(urls.booking),
-      fetch(urls.eventCurrent),
+      fetch(urls.eventsCurrent),
       fetch(urls.eventsRepeat),
     ])
       .then(function(allResponses){
@@ -55,7 +50,6 @@ export class Booking {
           eventsRepeatResponse.json(),
         ]);
       })
-      // Odpowiedz na prozbe 
       .then(function([booking, eventCurrent, eventsRepeat]){
         console.log(booking);
         console.log(eventCurrent);
@@ -64,21 +58,19 @@ export class Booking {
       });
   }
 
-  parseData(booking, eventCurrent, eventsRepeat){
+  parseData(bookings, eventsCurrent, eventsRepeat) {
     const thisBooking = this;
- 
+
     thisBooking.booked = {};
 
-    // Pozycja rezerwacji (data, godzina, czas trwania, stolik)
-    for(let item of booking){
-      thisBooking.makeBooked(item.data, item.hour, item.duration, item.table);
+    for (let item of bookings) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
     }
- 
-    // Sprawdzenie pozycji rezerwacji, czy jest mozliwa?
-    for(let item of eventCurrent){
-      thisBooking.makeBooked(item.data, item.hour, item.duration, item.table);
+
+    for (let item of eventsCurrent) {
+      thisBooking.makeBooked(item.date, item.hour, item.duration, item.table);
     }
- 
+
     const minDate = thisBooking.datePicker.minDate;
     const maxDate = thisBooking.datePicker.maxDate;
  
@@ -95,27 +87,26 @@ export class Booking {
     thisBooking.updateDOM();
   }
  
-  // Jakie dokladnie zadanie ma ta funkcja?
   makeBooked(date, hour, duration, table) {
     const thisBooking = this;
- 
-    // Co oznacza 'typeof'? Skąd jest ta 'date'?
+
     if (typeof thisBooking.booked[date] == 'undefined') {
       thisBooking.booked[date] = {};
     }
- 
-    const startHour = utils.numberToHour(hour);
- 
+
+    const startHour = utils.hourToNumber(hour);
+
     for (
       let hourBlock = startHour;
       hourBlock < startHour + duration;
       hourBlock += 0.5
     ) {
       // console.log('loop', hourBlock);
+
       if (typeof thisBooking.booked[date][hourBlock] == 'undefined') {
         thisBooking.booked[date][hourBlock] = [];
       }
- 
+
       thisBooking.booked[date][hourBlock].push(table);
     }
   }
@@ -154,18 +145,16 @@ export class Booking {
     }
   }
 
-  render(bookingContainer){
+  render(wrapper) {
     const thisBooking = this;
- 
+
     const generatedHTML = templates.bookingWidget();
- 
+
     thisBooking.dom = {};
- 
-    thisBooking.dom.wrapper = bookingContainer;
- 
-    thisBooking.dom.wrapper = utils.createDOMFromHTML(generatedHTML);
- 
-    bookingContainer.appendChild(thisBooking.dom.wrapper);
+
+    thisBooking.dom.wrapper = wrapper;
+
+    thisBooking.dom.wrapper.innerHTML = generatedHTML;
  
     thisBooking.dom.peopleAmount = thisBooking.dom.wrapper.querySelector(select.booking.peopleAmount);
     console.log(thisBooking.dom.peopleAmount);
